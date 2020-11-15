0b1fb7da6ccda56e0679cc12dfb9ca33d061da45
muhammadibrar1 <muhammadibrar1@yahoo.com>
2020-11-15 20:51:56 +0500

    update structure
    By Ibrar

--- _routes/auth.js
+++ _routes/auth.js
@@ -1,1 +1,22 @@
 'use strict'
+
+const express = require('express');
+
+
+
+const router = express.Router();
+
+router
+
+// Post login
+// /api/login
+.post('/login',   )
+
+
+// Post signUp
+// /api/signUp
+.post('/signUp', )
+
+// get User By Id
+// /api/login
+.get('/login', )
--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Routes
+const authRoutes = require('./_routes/auth')
+
+// End Routes
 
 const {PORT} = require('./env');
 
@@ -14,8 +18,18 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.use(bodyParser.json({limit: '150mb'}));
-// app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json({limit: '150mb'}));
+app.use(bodyParser.urlencoded({ extended: false}))
+
+
+
+
+// Routes Started 
+// Register Routes
+app.use('/api', authRoutes);
+
+// Routes End
+
 
 app.use((req, res, next) => {
     res.status(404).json('404 Api not found');
